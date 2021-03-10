81f883f2c058315934fb9510c8c5091f8db955d5
Dhevan Lau <dhevan999@gmail.com>
2021-03-10 20:32:09 +0200

    CameraController implemented.

---
+++ Colour Code/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform playerT;
+	public float followTime = 0.15f;
+	public float FollowSpeed = 2f;
+
+    public int heightIncrease = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+
+
+	void Update()
+	{
+		Vector3 newPosition = playerT.position;
+		newPosition.z = -10;
+        newPosition.y += heightIncrease;
+		this.transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+	}
+}
