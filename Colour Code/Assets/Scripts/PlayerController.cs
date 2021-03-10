1e37118d5d4c93e57b54131537c6bb06135e1042
Dhevan Lau <dhevan999@gmail.com>
2021-03-10 18:01:47 +0200

    Fixed Jumping with Colour changing mechanics

--- Colour Code/Assets/Scripts/PlayerController.cs
+++ Colour Code/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour
 		playerT = this.gameObject.GetComponent<Transform> ();
 		playerSR = this.gameObject.GetComponent<SpriteRenderer> ();
  		playerColour = Colour.neutral;
+		this.gameObject.layer = 3; //white
 	}
 
     // Update is called once per frame
@@ -50,23 +51,31 @@ public class PlayerController : MonoBehaviour
 	void checkMovement()
 	{
 		float horizontalMovement = Input.GetAxis ("Horizontal") * speed;
-		//float verticalMovement = Input.GetAxis ("Vertical") * speed;
-
 		Vector3 movement = new Vector3 (horizontalMovement, 0f, 0f) * Time.deltaTime;
-
 		this.transform.Translate(movement, Space.World);
+		this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0);
 	}
 
 	void checkRotation(){
 		transform.rotation = Quaternion.Euler(0, 0, 0);
 	}
 
+
+	public void EnableJump(){
+		canJump = true;
+	}
+
+	public void DisableJump(){
+		canJump = false;
+	}
+
 	void checkJump()
 	{
-		canJump = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+		//canJump = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
 		if(Input.GetKeyDown(KeyCode.Space) && canJump == true){
 			playerRB.AddForce(new Vector2(0,jumpHeight), ForceMode2D.Impulse);
+			canJump = false;
 		}
 	}
 
@@ -75,18 +84,26 @@ public class PlayerController : MonoBehaviour
 			case "Blue":
 				playerSR.color = new Color(0, 0, 1f, 1f); //Blue
 				playerColour = Colour.blue;
+				this.gameObject.layer = 7; //blue
+				GameObject.Find("GroundCheck").layer = 7;
 				break;
 			case "Red":
 				playerSR.color = new Color(1f, 0, 0f, 1f); //Red
 				playerColour = Colour.red;
+				this.gameObject.layer = 8; //red
+				GameObject.Find("GroundCheck").layer = 8;
 				break;
 			case "Yellow":
 				playerSR.color = new Color(1f, 1f, 0f, 1f); //Blue
 				playerColour = Colour.yellow;
+				this.gameObject.layer = 9; //yellow
+				GameObject.Find("GroundCheck").layer = 9;
 				break;
 			default:
-				Debug.Log("Incorrect colour option");
+				Debug.Log("Incorrect colour option - This should never appear.");
 				playerColour = Colour.neutral;
+				this.gameObject.layer = 6; 
+				GameObject.Find("GroundCheck").layer = 6;
 				break;
 		}
 	}
