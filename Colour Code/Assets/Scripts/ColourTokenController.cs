37d062e8888eebe1a8e2939d4ac5c709e8b73c83
Dhevan Lau <dhevan999@gmail.com>
2021-03-17 14:53:42 +0200

    Added delay to colour change token

--- Colour Code/Assets/Scripts/ColourTokenController.cs
+++ Colour Code/Assets/Scripts/ColourTokenController.cs
@@ -11,21 +11,32 @@ public class ColourTokenController : MonoBehaviour
     public float rotationSpeed;
     public Material blueMat, redMat, yellowMat;
 
+    private float delayTokenChangeTime = 1.5f;
+
+    bool colourChangeEnabled = true;
+
+    private float startTime;
+
     private Transform tokenT;
     // Start is called before the first frame update
     void Start()
     {
         tokenT = this.gameObject.GetComponent<Transform>();
+        startTime = Time.time;
+        colourChangeEnabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         tokenT.rotation = Quaternion.Euler((Time.time * rotationSpeed), 0f, (Time.time * rotationSpeed));
+        if(Time.time >= startTime + delayTokenChangeTime){
+            colourChangeEnabled = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.tag == "Player"){
+        if(other.gameObject.tag == "Player" && colourChangeEnabled == true){
             GameObject player = other.gameObject;
             string playerColour = player.GetComponent<PlayerController>().getColour();
 
@@ -41,7 +52,8 @@ public class ColourTokenController : MonoBehaviour
                     break;
             }
             changeTokenColour(playerColour);
-            
+            startTime = Time.time;
+            colourChangeEnabled = false;
         }
     }
 
