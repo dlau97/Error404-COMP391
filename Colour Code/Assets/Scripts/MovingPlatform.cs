55c7131e5d77226210a20e0cc276f32f573d39ef
Dhevan Lau <dhevan999@gmail.com>
2021-03-17 14:43:00 +0200

    Adjusted moving platforms

--- Colour Code/Assets/Scripts/MovingPlatform.cs
+++ Colour Code/Assets/Scripts/MovingPlatform.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Vector3 pos1;
-    public Vector3 pos2;
+    public enum MoveType {horizontal, vertical};
+    public MoveType type = MoveType.horizontal;
+    private Vector3 pos1;
+    private Vector3 pos2;
     public float speed = 1.0f;
+    public float moveDistance = 2f;
+
+
   
+    void Start() 
+    {
+        pos1 = this.transform.position;
+        pos2 = this.transform.position;
+
+        if(type ==MoveType.horizontal){
+            pos1.x -= moveDistance;
+            pos2.x += moveDistance;
+        }
+        else if(type == MoveType.vertical){
+            pos1.y -= moveDistance;
+            pos2.y += moveDistance;
+        }
+    }
 
     // Update is called once per frame
     void Update()
