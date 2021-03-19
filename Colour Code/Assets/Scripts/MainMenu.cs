963f302320809800279b9cdde238d0ca301f47b4
Dhevan Lau <dhevan999@gmail.com>
2021-03-19 11:51:26 +0200

    Added Screenshake, connected main menu to game scene. Alpha version done

--- Colour Code/Assets/Scripts/MainMenu.cs
+++ Colour Code/Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -18,15 +19,15 @@ public class MainMenu : MonoBehaviour
 
     public void OnClickNewGame()
     {
-        Debug.Log("start game");
+        SceneManager.LoadScene("Game Scene");
     }
     public void OnClickQuit()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
     }
     public void OnClickCredit()
     { 
