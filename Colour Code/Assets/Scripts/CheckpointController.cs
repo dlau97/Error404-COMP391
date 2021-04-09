e42a3f13075a72566b72530ed299788c666528eb
Dhevan Lau <dhevan999@gmail.com>
2021-04-09 16:56:46 +0200

    Added launchpad particle FX

--- Colour Code/Assets/Scripts/CheckpointController.cs
+++ Colour Code/Assets/Scripts/CheckpointController.cs
@@ -6,10 +6,13 @@ public class CheckpointController : MonoBehaviour
 {
     private SpriteRenderer checkpointSR;
 
+    private bool checkpointReached = false;
+
     // Start is called before the first frame update
     void Start()
     {
         checkpointSR = this.gameObject.GetComponent<SpriteRenderer>();
+        checkpointReached = false;
     }
 
     // Update is called once per frame
@@ -18,11 +21,13 @@ public class CheckpointController : MonoBehaviour
         
     }
         private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.tag == "Player"){
+        if(other.gameObject.tag == "Player" && checkpointReached == false){
+            checkpointReached = true;
             GameObject.Find("GameController").SendMessage("setCheckpoint", this.gameObject.transform.position);
-            checkpointSR.color =  new Color(0f, 1f, 0f, 1f); //Green
+            checkpointSR.color =  new Color(0.2684663f, 1f, 0.2301887f, 1f); //Green
             Debug.Log("Checkpoint: "+ this.gameObject.transform.position);
             GameObject.Find("GameController").SendMessage("PlayCheckpointSFX");
         }
     }
 }
+
